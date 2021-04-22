d6f55479013630f62ec8c8d34588a90338ec62b3
Arielwz <arieldududu@gmail.com>
2021-04-22 16:30:07 -0700

    solved upload page axe test issue

--- front/src/Pages/UploadPage.js
+++ front/src/Pages/UploadPage.js
@@ -6,7 +6,7 @@ export default function UploadPage(props) {
   return (
     <div className="col-4">
       <br />
-      <h2 className="create-info">Create a new Trip</h2>
+      <header><h1 className="create-info" style={{fontSize: "40px"}}>Create a new Trip</h1></header>
       {!!errorText && (
         <div
           id="error"
@@ -17,87 +17,93 @@ export default function UploadPage(props) {
           {errorText}
         </div>
       )}
-      <form
-        id="formCreate"
-        onSubmit={async (ev) => {
-          ev.preventDefault();
-          const formData = new FormData();
-          Object.entries(tripInfo).forEach(([key, value]) => {
-            formData.append(key, value);
-          });
-          const response = await fetch("/createTrip", {
-            method: "POST",
-            credentials: "same-origin",
-            body: formData,
-          });
-          const res = await response.json();
-          if (res && res.success) {
-            props.history.push("/");
-          } else {
-            setErrorText(res.message || "Upload failed!");
-          }
-        }}
-      >
-        <label className="form-label">
-          Title:{" "}
-          <input
-            className="form-control"
-            type="text"
-            onChange={(ev) => {
-              setTripInfo({
-                ...tripInfo,
-                name: ev.target.value,
-              });
-            }}
-          />
-        </label>
-        <br />
-        <label className="form-label">
-          Location:
-          <input
-            className="form-control"
-            type="text"
-            onChange={(ev) => {
-              setTripInfo({
-                ...tripInfo,
-                location: ev.target.value,
-              });
-            }}
-          />
-        </label>
-        <label className="form-label">
-          Description:
-          <br></br>
-          <textarea
-            rows="5"
-            cols="50"
-            onChange={(ev) => {
-              setTripInfo({
-                ...tripInfo,
-                content: ev.target.value,
-              });
-            }}
-          ></textarea>
-        </label>
-        <label className="form-label">
-          Photo:{" "}
-          <input
-            className="form-control"
-            type="file"
-            id="image-upload"
-            onChange={(ev) => {
-              setTripInfo({
-                ...tripInfo,
-                image: ev.target.files && ev.target.files[0],
-              });
-            }}
-          />
-        </label>
-        <br />
-        <button type="submit" className="btn btn-primary">
-          Create
-        </button>
-      </form>
+      <main>
+        <form
+          id="formCreate"
+          onSubmit={async (ev) => {
+            ev.preventDefault();
+            const formData = new FormData();
+            Object.entries(tripInfo).forEach(([key, value]) => {
+              formData.append(key, value);
+            });
+            const response = await fetch("/createTrip", {
+              method: "POST",
+              credentials: "same-origin",
+              body: formData,
+            });
+            const res = await response.json();
+            if (res && res.success) {
+              props.history.push("/");
+            } else {
+              setErrorText(res.message || "Upload failed!");
+            }
+          }}
+        >
+          <label className="form-label">
+            Title:{" "}
+            <input
+              className="form-control"
+              type="text"
+              aria-label="input title"
+              onChange={(ev) => {
+                setTripInfo({
+                  ...tripInfo,
+                  name: ev.target.value,
+                });
+              }}
+            />
+          </label>
+          <br />
+          <label className="form-label">
+            Location:
+            <input
+              className="form-control"
+              type="text"
+              aria-label="input location"
+              onChange={(ev) => {
+                setTripInfo({
+                  ...tripInfo,
+                  location: ev.target.value,
+                });
+              }}
+            />
+          </label>
+          <label className="form-label">
+            Description:
+            <br></br>
+            <textarea
+              rows="5"
+              cols="50"
+              aria-label="input description"
+              onChange={(ev) => {
+                setTripInfo({
+                  ...tripInfo,
+                  content: ev.target.value,
+                });
+              }}
+            ></textarea>
+          </label>
+          <label className="form-label">
+            Photo:{" "}
+            <input
+              className="form-control"
+              type="file"
+              id="image-upload"
+              aria-label="choose photo"
+              onChange={(ev) => {
+                setTripInfo({
+                  ...tripInfo,
+                  image: ev.target.files && ev.target.files[0],
+                });
+              }}
+            />
+          </label>
+          <br />
+          <button type="submit" className="btn btn-primary" aria-label="submit">
+            Create
+          </button>
+        </form>
+      </main>
     </div>
   );
 }
