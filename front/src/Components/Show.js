b6465b0117b9480ed8255e29191a25d24b758719
qin0511 <xyqin2017@gmail.com>
2021-04-26 11:48:44 -0700

    Fixed the space of Show.js

--- front/src/Components/Show.js
+++ front/src/Components/Show.js
@@ -3,29 +3,29 @@ import PropTypes from "prop-types";
 import Trip from "./Trip";
 
 function Show(props) {
-	return props.trips ? (
-		<div className="container">
-			<div className="row">
-				<div className="col-6 col-sm" id="card-display">
-					{props.selectedTripObj ? (
-						<Trip trip={props.selectedTripObj} key={props.trip} />
-					) : (
-						props.trips.map((t) => (
-							<div className="card-deck">
-								<div className="card-body">
-									<Trip key={t.id} trip={t} />
-								</div>
-							</div>
-						))
-					)}
-				</div>
-			</div>
-		</div>
-	) : null;
+  return props.trips ? (
+    <div className="container">
+      <div className="row">
+        <div className="col-6 col-sm" id="card-display">
+          {props.selectedTripObj ? (
+            <Trip trip={props.selectedTripObj} key={props.trip} />
+          ) : (
+            props.trips.map((t) => (
+              <div className="card-deck">
+                <div className="card-body">
+                  <Trip key={t.id} trip={t} />
+                </div>
+              </div>
+            ))
+          )}
+        </div>
+      </div>
+    </div>
+  ) : null;
 }
 
 Show.propTypes = {
-	props: PropTypes.array,
+  props: PropTypes.array,
 };
 
 export default Show;
